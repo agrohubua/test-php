de90eec2335ffb7997ed4e901207b8092cb51d08
Dmytro Skintiian <Dmytro.Skintiian@ferm.in.ua>
2024-06-07 16:56:29 +0300

    auth codes details

--- src/Security/UserInterface.php
+++ src/Security/UserInterface.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Security;
 
+use Iterator;
 use Security\Enum\TwoFaTypeEnum;
 
 interface UserInterface
 {
     public function getPhone(): string;
+    public function getAuthCodes(): Iterator;
     public function get2FaType(): TwoFaTypeEnum;
 }
